5518d6a81636e9b8b0dc0fa23688a7c7325e2c8c
avardag <steklofan@gmail.com>
2021-05-20 17:38:42 +0200

    created a reusable component with tailwind classes in JS

---
+++ main.js
@@ -0,0 +1,43 @@
+import popularDestinations from "./popularDestinations";
+
+export function DestinationCard(destination) {
+  return `<div
+    class="flex items-center rounded-lg bg-white shadow-lg overflow-hidden"
+  >
+    <img
+      class="h-32 w-32 flex-shrink-0 object-cover object-center"
+      src="${destination.imageUrl}"
+      alt="${destination.imageAlt}"
+    />
+    <div class="px-6 py-4">
+      <h3 class="text-lg font-semibold text-gray-800">
+        ${destination.city}
+      </h3>
+
+      <p class="text-gray-600">
+        ${destination.averagePrice} / night average
+      </p>
+      <div class="mt-4">
+        <a
+          href="#"
+          class="text-indigo-600 hover:text-indigo-500 font-semibold text-sm"
+        >
+          Explore ${destination.propertyCount} properties
+        </a>
+      </div>
+    </div>
+  </div> `;
+}
+
+const renderList = (destinations) =>
+  destinations.map((dest) => `${DestinationCard(dest)}`);
+
+function DestinationList(destinations) {
+  return `<div class="mt-6 grid gap-6 lg:grid-cols-2 xl:grid-cols-3">
+    ${renderList(destinations).join("")}
+  </div>`;
+}
+
+document
+  .getElementById("root")
+  .insertAdjacentHTML("afterbegin", DestinationList(popularDestinations));
---
+++ popularDestinations.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    city: "Toronto",
+    averagePrice: 120,
+    propertyCount: 76,
+    imageUrl: "https://loremflickr.com/320/240/toronto",
+    imageAlt: "Toronto skyline",
+  },
+  {
+    city: "Malibu",
+    averagePrice: 215,
+    propertyCount: 43,
+    imageUrl: "https://loremflickr.com/320/240/malibu",
+    imageAlt: "Cliff in Malibu",
+  },
+  {
+    city: "Chicago",
+    averagePrice: 130,
+    propertyCount: 115,
+    imageUrl: "https://loremflickr.com/320/240/chicago",
+    imageAlt: "Chicago skyline",
+  },
+  {
+    city: "Seattle",
+    averagePrice: 135,
+    propertyCount: 63,
+    imageUrl: "https://loremflickr.com/320/240/seattle",
+    imageAlt: "Seattle skyline",
+  },
+  {
+    city: "Colorado",
+    averagePrice: 85,
+    propertyCount: 47,
+    imageUrl: "https://loremflickr.com/320/240/colorado",
+    imageAlt: "Lake in Colorado",
+  },
+  {
+    city: "Miami",
+    averagePrice: 115,
+    propertyCount: 86,
+    imageUrl: "https://loremflickr.com/320/240/miami",
+    imageAlt: "Beach in Miami",
+  },
+];
